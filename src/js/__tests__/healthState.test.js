eafca4430d8ccbffce4f95ca5f2c72774ebd7f1d
“Andrey <gordov.ar@gmail.com>
2021-11-25 14:43:44 +0500

    1.0.0

---
+++ src/js/__tests__/healthState.test.js
@@ -0,0 +1,14 @@
+import healthState from '../healthState';
+
+test.each([
+  [95, 'healthy'],
+  [50, 'healthy'],
+  [49.9, 'wounded'],
+  [15, 'wounded'],
+  [14.9, 'critical'],
+  [5, 'critical'],
+])('health test', (health, expected) => {
+    const result = healthState({name: 'Маг', health});
+  
+    expect(result).toBe(expected);
+  });
---
+++ src/js/healthState.js
@@ -0,0 +1,13 @@
+function healthState(item) {
+  if (item.health >= 50) {
+    return 'healthy';
+  }
+  if (item.health < 50 && item.health >= 15) {
+    return 'wounded';
+  }
+  if (item.health < 15) {
+    return 'critical';
+  }
+}
+
+export default healthState;
